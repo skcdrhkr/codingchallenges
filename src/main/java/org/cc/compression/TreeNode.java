519f089b95a4b59fe5591e9a873e36e9dcb2694b
Shailendra Kumar <20299310+skcdrhkr@users.noreply.github.com>
2024-08-29 22:46:34 +0530

    Huffman Encoding Tree and Prefix Codes with Tests

---
+++ src/main/java/org/cc/compression/TreeNode.java
@@ -0,0 +1,36 @@
+package org.cc.compression;
+
+public class TreeNode {
+    int weight;
+    Character data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+        weight = -1;
+        data = null;
+        left = null;
+        right = null;
+    }
+
+    public TreeNode(int weight) {
+        this.weight = weight;
+        this.data = null;
+        left = null;
+        right = null;
+    }
+
+    public TreeNode(int weight, Character data) {
+        this.weight = weight;
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public TreeNode(int weight, TreeNode left, TreeNode right) {
+        this.weight = weight;
+        this.data = null;
+        this.left = left;
+        this.right = right;
+    }
+}
